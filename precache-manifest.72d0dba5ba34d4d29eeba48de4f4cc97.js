c4fd785b453f878e87c5577cc6444332f43681f4
ZYA-E480 <annzhang520@qq.com>
2019-05-16 18:26:58 +0800

    Updates

---
+++ precache-manifest.72d0dba5ba34d4d29eeba48de4f4cc97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4eb80f820f2fb67b9f67a9de2e59520",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "ddca499931db2c129a82",
+    "url": "/react-tutorial/static/css/main.545ae54c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "ddca499931db2c129a82",
+    "url": "/react-tutorial/static/js/main.22add563.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
